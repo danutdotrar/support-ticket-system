2d412d5bad8b41896693786eb4c038b0666016dd
danutdotrar <morarasu.danut@gmail.com>
2024-01-18 14:51:07 +0200

    Add basic express server config

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,14 @@
-// entry point to backend
-console.log("--- server ---");
+// Initialize express
+const express = require("express");
+
+// Initialize app
+const app = express();
+
+// Initialize dotenv
+const dotenv = require("dotenv").config();
+
+// Define the port
+const PORT = process.env.PORT || 8000;
+
+// Listen to a port
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
