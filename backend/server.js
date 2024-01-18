2350874b5c65a40ac2b8e90ff8ee020ed778edd9
danutdotrar <morarasu.danut@gmail.com>
2024-01-18 15:14:31 +0200

    Add /api/users + ... /login

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,13 @@ const dotenv = require("dotenv").config();
 // Define the port
 const PORT = process.env.PORT || 8000;
 
-// Listen to a port
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
 // Create a GET route with express
-app.get("/api/users", (req, res) => {
+app.get("/", (req, res) => {
     res.status(200).json({ message: "Welcome to the support desk API" });
 });
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+// Listen to a port
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
