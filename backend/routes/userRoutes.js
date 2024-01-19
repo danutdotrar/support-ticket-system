601389af164b2ec17636b9799187a104f72efb31
danutdotrar <morarasu.danut@gmail.com>
2024-01-19 16:38:08 +0200

    Add auth middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, loginUser } = require("../controllers/userController");
+const {
+    registerUser,
+    loginUser,
+    getMe,
+} = require("../controllers/userController");
+
+const { protect } = require("../middleware/authMiddleware");
 
 // register
 router.post("/", registerUser);
 
 // login
 router.post("/login", loginUser);
+
+router.get("/me", protect, getMe);
 
 module.exports = router;
