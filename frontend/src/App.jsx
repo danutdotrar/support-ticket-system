bdd61666982910bd2dad6b68fe8119db8de8fb7a
danutdotrar <morarasu.danut@gmail.com>
2024-01-22 13:42:47 +0200

    Add header structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -6,6 +7,9 @@ import Register from "./pages/Register";
 const App = () => {
     return (
         <>
+            <div className="container">
+                <Header />
+            </div>
             <Router>
                 <Routes>
                     <Route path="/" element={<Home />} />
