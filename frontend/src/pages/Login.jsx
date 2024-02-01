8a7d967a27a0a754b881d144aa8cb0b5ef264770
danutdotrar <morarasu.danut@gmail.com>
2024-02-01 14:53:08 +0200

    Add login fn

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaSignInAlt } from "react-icons/fa";
+import Spinner from "../components/Spinner";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { login } from "../features/auth/authSlice";
+import { login, reset } from "../features/auth/authSlice";
 
 const Login = () => {
     const [formData, setFormData] = useState({
@@ -14,10 +16,24 @@ const Login = () => {
     const { email, password } = formData;
 
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-    const { user, isLoading, isSuccess, message } = useSelector(
+    const { user, isLoading, isSuccess, isError, message } = useSelector(
         (state) => state.auth
     );
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+
+        // redirect when logged in
+        if (isSuccess || user) {
+            navigate("/");
+        }
+
+        dispatch(reset());
+    }, [isError, isSuccess, user, message, navigate, dispatch]);
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -36,6 +52,10 @@ const Login = () => {
 
         dispatch(login(userData));
     };
+
+    if (isLoading) {
+        return Spinner;
+    }
 
     return (
         <>
