bc843f5643164f09a65cd8449591c805c440a9f4
danutdotrar <morarasu.danut@gmail.com>
2024-02-06 18:28:59 +0200

    Add gettickets

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -2,29 +2,43 @@ const asyncHandler = require("express-async-handler");
 
 const User = require("../models/userModel");
 const Ticket = require("../models/ticketModel");
+const { compare } = require("bcryptjs");
 
 // @description Get current tickets
 // @route GET /api/tickets
 // @access Private
 const getTickets = asyncHandler(async (req, res) => {
-    const ticket = {
-        id: req.user._id,
-        email: req.user.email,
-        name: req.user.name,
-    };
+    // get user using the id in the JWT
+    const user = await User.findById(req.user.id);
 
-    res.status(200).json({ message: "get tickets" });
+    if (!user) {
+        res.status(401);
+        throw new Error("User not found");
+    }
+
+    const tickets = await Ticket.find({ user: req.user.id });
+
+    res.status(200).json({ message: "tickets" });
 });
 
 // @description Create new tickets
 // @route POST /api/tickets/
 // @access Private
 const createTicket = asyncHandler(async (req, res) => {
-    const newTicket = {
-        id: req.user._id,
-        email: req.user.email,
-        name: req.user.name,
-    };
+    const { product, description } = req.body;
+
+    if (!product | !description) {
+        res.status(400);
+        throw new Error("Please add a product and description");
+    }
+
+    // get user using the id in the JWT
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        res.status(401);
+        throw new Error("User not found");
+    }
 
     res.status(200).json({ message: "Create tickets" });
 });
