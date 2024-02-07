ed9a432dffdf9026ff438dd2dea208a7170a7ad5
danutdotrar <morarasu.danut@gmail.com>
2024-02-07 15:23:45 +0200

    Add useAuthStatus hook functionality

--- frontend/src/hooks/useAuthStatus.js
+++ frontend/src/hooks/useAuthStatus.js
@@ -3,8 +3,20 @@ import { useSelector } from "react-redux";
 
 export const useAuthStatus = () => {
     const [loggedIn, setLoggedIn] = useState(false);
-    const [checkingStatus, setCheckingStatue] = useState(true);
+    const [checkingStatus, setCheckingStatus] = useState(true);
 
     // get user from the state
     const { user } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (user) {
+            setLoggedIn(true);
+        } else {
+            setLoggedIn(false);
+        }
+
+        setCheckingStatus(false);
+    }, [user]);
+
+    return { loggedIn, checkingStatus };
 };
