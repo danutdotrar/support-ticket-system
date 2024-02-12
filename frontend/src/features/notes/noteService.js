482f484eec27901b98134cba78b1915686d7b4bb
danutdotrar <morarasu.danut@gmail.com>
2024-02-12 14:48:24 +0200

    Fix api url form noteservice

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -1,1 +1,18 @@
-const createNote = async (req, res) => {};
+import axios from "axios";
+
+const API_URL = "/api/tickets/";
+
+// Get ticket notes
+const getNotes = async (ticketId, token) => {
+    // the token must match in Authorization header
+    const config = {
+        headers: { Authorization: `Bearer ${token}` },
+    };
+
+    const response = await axios.get(API_URL + ticketId + "/notes", config);
+
+    return response.data;
+};
+
+const noteService = { getNotes };
+export default noteService;
--- frontend/src/features/notes/noteSlice.js
+++ frontend/src/features/notes/noteSlice.js
@@ -1,21 +1,23 @@
 // we need createSlice and createAsyncThunk from redux toolkit
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import noteService from "../notes/noteService";
+import noteService from "./noteService";
 
 // Create initial state
 const initialState = {
-    user: "",
-    ticket: "",
-    text: "",
+    notes: [],
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: "",
 };
 
-// Create new note
-export const createNote = createAsyncThunk(
-    "notes/create",
-    async (noteData, thunkAPI) => {
+// Get notes
+export const getNotes = createAsyncThunk(
+    "notes/getAll",
+    async (ticketId, thunkAPI) => {
         try {
             const token = thunkAPI.getState().auth.user.token;
-            return await noteService.createNote(noteData, token);
+            return await noteService.getNotes(ticketId, token);
         } catch (error) {
             const message =
                 (error.response &&
@@ -28,3 +30,31 @@ export const createNote = createAsyncThunk(
         }
     }
 );
+
+// Create noteSlice
+export const noteSlice = createSlice({
+    name: "note",
+    initialState,
+    reducers: {
+        reset: (state) => initialState,
+        extraReducers: (builder) => {
+            builder
+                .addCase(getNotes.pending, (state) => {
+                    state.isLoading = true;
+                })
+                .addCase(getNotes.fulfilled, (state, action) => {
+                    state.isLoading = false;
+                    state.isSuccess = true;
+                    state.ticket = action.payload;
+                })
+                .addCase(getNotes.rejected, (state, action) => {
+                    state.isLoading = false;
+                    state.isError = true;
+                    state.message = action.payload;
+                });
+        },
+    },
+});
+
+export const { reset } = noteSlice.actions;
+export default noteSlice.reducer;
